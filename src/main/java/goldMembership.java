1bbfdada572e7365caf75a9209f5ff568d815a35
Jonathan Løseth <jonathhl@stud.ntnu.no>
2021-02-01 18:27:35 +0100

    Added a couple of methods and attributes to the different classes, will flesh them out more later.

--- src/main/java/goldMembership.java
+++ src/main/java/goldMembership.java
@@ -1,2 +1,6 @@
 public class goldMembership extends membership {
+
+    public int registerPoints(int bonusPointBalance, int newPoints) {
+
+    }
 }
--- src/main/java/membership.java
+++ src/main/java/membership.java
@@ -1,3 +1,12 @@
 public abstract class membership {
 
+    private int totalPoints = 0;
+
+    int registerPoints(int bonusPointBalance, int newPoints) {
+        return totalPoints;
+    }
+
+    String getMembershipName() {
+
+    }
 }
