cb3e0bbd252858bedbb42a837123a10be250547f
jonathhl <70698120+jonathhl@users.noreply.github.com>
2021-02-04 16:32:01 +0100

    Delete basicMembership.java

--- src/main/java/basicMembership.java
+++
@@ -1,9 +0,0 @@
-public class basicMembership extends membership {
-
-    private float POINTS_SCALING_FACTOR_LEVEL_1 = 1.3f;
-    private float POINTS_SCALING_FACTOR_LEVEL_2 = 1.5f;
-
-    public int registerPoints(int bonusPointBalance, int newPoints) {
-
-    }
-}
